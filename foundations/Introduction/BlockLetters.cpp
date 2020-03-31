7dc1cacafb4262e3329ad3c6d26bf2bbb14130f1
Kelli Davis <keldavis5@yahoo.com>
2020-03-31 00:33:36 -0400

    feat: add program that prints initials in block letters

---
+++ foundations/Introduction/BlockLetters.cpp
@@ -0,0 +1,28 @@
+// A program that writes my initials in block letters
+#include <iostream>
+
+int main() {
+  std::cout << "k   k\nk  k \nk k  \nkk   \nk k  \nk k  \nk  k \nk   k\n";
+  std::cout << "dddd \nd   d\nd    d\nd    d\nd    d\nd   d\ndddd ";
+}
+
+/**
+Output:
+
+k   k
+k  k
+k k
+kk
+k k
+k k
+k  k
+k   k
+
+dddd
+d   d
+d    d
+d    d
+d    d
+d   d
+dddd
+*/
