f16b2fef61d2ed2c65c232f64117117031fd5fae
Kelli Davis <keldavis5@yahoo.com>
2020-04-06 15:10:04 -0400

    feat: add dog years conversion program

---
+++ foundations/Introduction/dog_years.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+  // My dog's age
+  int dog_age = 5;
+  
+  int early_years, later_years, human_years;
+  
+  // The first two years of a dog’s life count as 21 human years
+  early_years = 21;
+  
+  // Each following year counts as 4 human years
+  later years = (dog_age - 2) * 4
+  
+  // my dogs age in human years
+  human_years = early_years + later_years;
+  
+  std::cout << "My name is Chris! Ruff ruff, I am " << human_years << " years old in human years\n"; 
+  
+}
