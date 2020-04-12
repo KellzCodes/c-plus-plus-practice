6eeb6bbfd0da430259646640cc088b5c3f818115
Kelli Davis <keldavis5@yahoo.com>
2020-04-12 17:51:28 -0400

    feat: add a program that demonstrates the use of logical ||

---
+++ foundations/2. Conditionals and Logic/or.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  
+  int day = 6;
+  
+  // If day is equal to 6 or day is equal to 7, then print the word "Weekend".
+  
+  if(day == 6 || day == 7){
+    std::cout << "Weekend\n";
+  }
+}
