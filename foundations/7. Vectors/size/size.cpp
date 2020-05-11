b95664d10451c2eb003fd5fdca9d86fc8ef839b9
Kelli Davis <keldavis5@yahoo.com>
2020-05-11 00:21:07 -0400

    feat: add a program that shows how to print the size of a vector

---
+++ foundations/7. Vectors/size/size.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+  
+  std::vector<std::string> grocery = {"Hot Pepper Jam", "Dragon Fruit", "Brussel Sprouts"};
+    
+  // Add more 
+  
+  grocery.push_back("artichoke");
+  grocery.push_back("celery");
+
+  // print grocery size
+  std::cout << grocery.size();
+  
+  
+  
+  
+}
