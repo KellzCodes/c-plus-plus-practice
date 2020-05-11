4775b18061477e3ced7b14da4871b00d6b2e33ae
Kelli Davis <keldavis5@yahoo.com>
2020-05-10 23:19:24 -0400

    feat: add a program that shows how to print a vector index

---
+++ foundations/7. Vectors/Index/tokyo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+   
+  std::vector<double> subway_adult = {800, 1200, 1500};
+  
+  std::vector<double> subway_child = {400, 600, 750};
+  
+  // What number at index 2 of subway_child?
+  std::cout << subway_child[2];
+  
+  
+}
