9ee87c9c4bea34709a2763a9ad25d4cc02f50784
Kelli Davis <keldavis5@yahoo.com>
2020-05-10 23:07:03 -0400

    feat: add a program that shows how to initialize a vector

---
+++ foundations/7. Vectors/Initializing a Vector/tokyo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+   
+  std::vector<double> subway_adult = {800, 1200, 1500};
+  
+  // Give subway_child some values:
+  
+  std::vector<double> subway_child = {400, 600, 750};
+  
+}
