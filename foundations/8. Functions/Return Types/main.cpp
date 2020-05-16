6af7aa99a2f95cd938e56ecf10de8b4f710dce2a
Kelli Davis <keldavis5@yahoo.com>
2020-05-16 00:31:48 -0400

    feat: add a program that shows how to use methods with return types

---
+++ foundations/8. Functions/Return Types/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+// Change needs_it_support so that it returns support:
+bool needs_it_support() {
+  
+  bool support;
+  
+  std::cout << "Hello. IT. Have you tried turning it off and on again? Enter 1 for yes, 0 for no.\n";
+  
+  return support;
+}
+
+int main() {
+  
+  // Change the following line to print the function result:
+  std::cout << needs_it_support();
+  
+}
