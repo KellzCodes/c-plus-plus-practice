b33bebde13a7eab88a914bec6db29b8574e09afd
Kelli Davis <keldavis5@yahoo.com>
2020-05-15 22:22:50 -0400

    feat: add sample code that needs functions

---
+++ foundations/8. Functions/Intro to Functions/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+  
+  // Conduct IT support
+  std::string on_off_attempt;
+  std::cout << "Hello. IT.\n";
+  std::cout << "Have you tried turning it off and on again? y/n\n";
+  std::cin >> on_off_attempt;
+  
+  // Check in with Jen
+  std::cout << "Oh hi Jen!\n";
+  
+  // Conduct IT support again...
+  std::cout << "Hello. IT.\n";
+  std::cout << "Have you tried turning it off and on again? y/n\n";
+  std::cin >> on_off_attempt;
+
+  // Check in with Roy
+  std::cout << "You stole the stress machine? But that's stealing!\n";
+  
+  // Conduct IT support yet again...zzzz...
+  std::cout << "Hello. IT.\n";
+  std::cout << "Have you tried turning it off and on again? y/n\n";
+  std::cin >> on_off_attempt;
+  
+}
