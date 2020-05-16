ee53e52dca6d85a3f1e0f2cfc383b07a7412a5cd
Kelli Davis <keldavis5@yahoo.com>
2020-05-16 00:07:37 -0400

    feat: add program that shows how to define a void function

---
+++ foundations/8. Functions/Void Method/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// Define oscar_wilde_quote() below:
+void oscar_wilde_quote(){
+  std::cout << "The highest, as the lowest, form of criticism is a mode of autobiography.";
+}
+
+int main() {
+  
+  // Call your function here:
+  oscar_wilde_quote();
+  
+}
