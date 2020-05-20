b2ee2cefd91c563d7feefbae6317a126139f027a
Kelli Davis <keldavis5@yahoo.com>
2020-05-20 00:42:43 -0400

    feat: add a program that shows how to use a header file

---
+++ foundations/8. Functions/Header Files/fns.hpp
@@ -0,0 +1,4 @@
+// Move function declarations here:
+double average(double num1, double num2);
+int tenth_power(int num);
+bool is_palindrome(std::string text);
---
+++ foundations/8. Functions/Header Files/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "fns.hpp"
+
+int main() {
+  
+  std::cout << is_palindrome("noon") << "\n";
+  std::cout << tenth_power(4) << "\n";
+  std::cout << average(4.0, 7.0) << "\n";
+  
+}
