b334e7dc4df06c3c6d63ed6570258537436c1a9d
Kelli Davis <keldavis5@yahoo.com>
2020-05-21 22:29:36 -0400

    feat: add a program that shows how to write function templates

---
+++ foundations/8. Functions/Function Templates/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "numbers.hpp"
+
+int main() {
+  
+  std::cout << get_smallest(100, 60) << "\n";
+  std::cout << get_smallest(2543.2, 3254.3) << "\n";
+  
+}
---
+++ foundations/8. Functions/Function Templates/numbers.hpp
@@ -0,0 +1,8 @@
+// Replace these declarations with a template
+
+template <typename T>
+T get_smallest(T num1, T num2) {
+  
+  return num2 < num1? num2 : num1;
+  
+}
