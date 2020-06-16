c97779d64130493efac450b7eb2d03640e8dd3c5
Kelli Davis <keldavis5@yahoo.com>
2020-06-16 09:04:32 -0400

    feat: add a program that demonstrates the using namespace declaration

---
+++ Sam's Teach Yourself/Part 1 The Basics/Lesson 2 The Anatomy of a C++ program/namespace.cpp
@@ -0,0 +1,14 @@
+// Preprocessor directive
+#include <iostream>
+
+// Start of your program
+int main(){
+	// Tell the compiler what namespace to search in
+	using namespace std;
+
+	/* Write to the screen using std::cout */
+	cout << "Hello World" << endl;
+
+	// Return a value to the OS
+	return 0;
+}
