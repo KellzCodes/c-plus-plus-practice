e600f0a1992295995bdc80fbcc450245963a17c8
Kelli Davis <keldavis5@yahoo.com>
2020-06-16 07:55:39 -0400

    chore: update fix

--- Sam's Teach Yourself/Part 1 The Basics/Lesson 2 The Anatomy of a C++ program/HelloWorldAnalysis.cpp
+++ Sam's Teach Yourself/Part 1 The Basics/Lesson 2 The Anatomy of a C++ program/HelloWorldAnalysis.cpp
@@ -1,1 +1,11 @@
-// Preprocessor
+// Preprocessor directive thaqt includes header iostream
+# include <iostream>
+
+// Start of your program: function block main()
+int main(){
+	/* Write to the screen */
+	std::cout << "Hello World" << std::endl;
+
+	// Return a value to the OS
+	return 0;
+}
