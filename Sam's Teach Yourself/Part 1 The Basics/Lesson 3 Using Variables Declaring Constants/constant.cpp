8f8b5482720067ce9a8c987d34cfa34d992392a0
Kelli Davis <keldavis5@yahoo.com>
2020-06-17 13:45:50 -0400

    feat: declaring a constant

---
+++ Sam's Teach Yourself/Part 1 The Basics/Lesson 3 Using Variables Declaring Constants/constant.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+	using namespace std;
+	
+	const double pi = 22.0 / 7;
+	cout << "The value of constant pi is: " << pi << endl;
+	
+	// Uncomment next line to view compile error
+	// pi = 345;
+	
+	return 0;
+}
