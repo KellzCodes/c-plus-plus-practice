91b1ddad459f18d58fe70af664a91e12632fa3c7
Kelli Davis <keldavis5@yahoo.com>
2020-06-27 18:32:17 -0400

    feat: Analyzing the Null-Terminator in a C-style string

---
+++ Sam's Teach Yourself/Part 1 The Basics/Lesson 4 Managing Arrays and Strings/NullTerminator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	char sayHello[] = {'H','e','l','l','o',' ','W','o','r','l','d','\0'};
+	cout << sayHello << endl;
+	cout << "Size of array: " << sizeof(sayHello) << endl;
+	
+	cout << "Replacing space with null" << endl;
+	sayHello[5] = '\0';
+	cout << sayHello << endl;
+	cout << "Size of array: " << sizeof(sayHello) << endl;
+	
+	return 0;
+}
