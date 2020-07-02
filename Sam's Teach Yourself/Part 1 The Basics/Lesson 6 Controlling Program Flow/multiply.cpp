6c598cb4b00f86dadd6606509fc57a8a7d00661b
Kelli Davis <keldavis5@yahoo.com>
2020-07-02 08:58:15 -0400

    feat: Multiplying or adding two integers on the basis of user input

---
+++ Sam's Teach Yourself/Part 1 The Basics/Lesson 6 Controlling Program Flow/multiply.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "Enter two integers: " << endl;
+	int num1 = 0, num2 = 0;
+	cin >> num1;
+	cin >> num2;
+	
+	cout << "Enter \'m\' to multiply, anything, anything else to add: ";
+	char userSelection = '\0';
+	cin >> userSelection;
+	
+	int result = 0;
+	if (userSelection == 'm')
+		result = num1 * num2;
+	else
+		result = num1 + num2;
+		
+	cout << "result is: " << result << endl;
+	
+	return 0;
+}
